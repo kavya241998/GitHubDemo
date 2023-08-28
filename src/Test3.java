2019660e1667cb69e76b48537134ff107e95140b
Hiremath <Hiremath@DESKTOP-UE27AH2>
2023-08-28 18:34:23 +0530

    performed the changes

---
+++ src/Test3.java
@@ -0,0 +1,18 @@
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="Hello";
+		if(s.equalsIgnoreCase("Hi"))
+		{
+			System.out.println("true");
+		}
+		else
+		{
+			System.out.println("false");
+		}
+
+	}
+
+}
